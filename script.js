b9662dbd10f1c886add096317de4c29fc0f9f3a1
CRY BABY <e.r.e.n.2.0.0.0.0.0.0.0.0.0.0@gmail.com>
2026-02-11 08:42:56 +0545

    update time function in js

---
+++ script.js
@@ -0,0 +1,14 @@
+let pride = document.getElementById("price")
+let time = document.getElementById("time")
+
+function Time(Clock) {
+    let date = new Date()
+    let hours = date.getHours()
+    let minutes = date.getMinutes()
+    let second = date.getSeconds()
+    Clock.textContent = `Time:- ${hours}:${minutes}:${second}`
+}
+Time(time)
+setInterval(() => {
+    Time(time)
+}, 1500);
